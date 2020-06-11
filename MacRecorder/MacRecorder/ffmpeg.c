e648d9dbd99cd7b1310b70ef5ba6134851f8179b
LiLi Kazine <lilikazine@gmail.com>
2020-06-11 14:18:40 +0800

    extract audio

--- MacRecorder/MacRecorder/ffmpeg.c
+++ MacRecorder/MacRecorder/ffmpeg.c
@@ -8,11 +8,64 @@
 
 #include "ffmpeg.h"
 
-static void adts_header(char *szAdtsHeader, int dataLen){
-    
-    int audio_object_type = 2;
-    int sampling_frequency_index = 7;
-    int channel_config = 2;
+
+#define ADTS_HEADER_LEN  7;
+
+static int get_audio_obj_type(int aactype) {
+    //AAC HE V2 = AAC LC + SBR + PS
+    //AAV HE = AAC LC + SBR
+    //所以无论是 AAC_HEv2 还是 AAC_HE 都是 AAC_LC
+    switch(aactype){
+        case 0:
+        case 2:
+        case 3:
+            return aactype+1;
+        case 1:
+        case 4:
+        case 28:
+            return 2;
+        default:
+            return 2;
+
+    }
+}
+
+static int get_sample_rate_index(int freq, int aactype) {
+
+    int i = 0;
+    int freq_arr[13] = {
+        96000, 88200, 64000, 48000, 44100, 32000,
+        24000, 22050, 16000, 12000, 11025, 8000, 7350
+    };
+
+    //如果是 AAC HEv2 或 AAC HE, 则频率减半
+    if(aactype == 28 || aactype == 4){
+        freq /= 2;
+    }
+
+    for(i=0; i< 13; i++){
+        if(freq == freq_arr[i]){
+            return i;
+        }
+    }
+    return 4;//默认是44100
+}
+
+static int get_channel_config(int channels, int aactype) {
+    //如果是 AAC HEv2 通道数减半
+    if(aactype == 28){
+        return (channels / 2);
+    }
+    return channels;
+}
+
+static void adts_header(char *szAdtsHeader, int dataLen, int aactype, int frequency, int channels) {
+
+    int audio_object_type = get_audio_obj_type(aactype);
+    int sampling_frequency_index = get_sample_rate_index(frequency, aactype);
+    int channel_config = get_channel_config(channels, aactype);
+
+//    printf("aot=%d, freq_index=%d, channel=%d\n", audio_object_type, sampling_frequency_index, channel_config);
 
     int adtsLen = dataLen + 7;
 
@@ -39,6 +92,7 @@ static void adts_header(char *szAdtsHeader, int dataLen){
     szAdtsHeader[5] |= 0x1f;                                 //buffer fullness:0x7ff 高5bits
     szAdtsHeader[6] = 0xfc;
 }
+
 void set_log_level(int level) {
     av_log_set_level(level);
 }
@@ -127,19 +181,38 @@ void extract_audio(const char *src, const char *dst) {
     
     if (ret < 0) {
         av_log(NULL, AV_LOG_ERROR, "Cannot find best stream: %s\n", av_err2str(ret));
-        avformat_close_input(&ctx);
-        fclose(audio_file);
-        return;
+        goto __ERROR;
     } else {
         audio_index = ret;
     }
+    
+    if (audio_index < 0) {
+        av_log(NULL, AV_LOG_DEBUG, "Could not find %s stream in input file %s\n",
+               av_get_media_type_string(AVMEDIA_TYPE_AUDIO),
+               src);
+        goto __ERROR;
+    }
+    
     av_init_packet(&pkt);
+    pkt.data = NULL;
+    pkt.size = 0;
+
+    int aac_type = ctx->streams[1]->codecpar->profile;
+    int channels = ctx->streams[1]->codecpar->channels;
+    int sample_rate = ctx->streams[1]->codecpar->sample_rate;
+    
+    if (ctx->streams[1]->codecpar->codec_id != AV_CODEC_ID_AAC) {
+        av_log(NULL, AV_LOG_ERROR, "the audio type is not AAC.\n");
+        goto __ERROR;
+    } else {
+        av_log(NULL, AV_LOG_INFO, "the audio type is AAC!\n");
+    }
 
     while (ret >= 0) {
         ret = av_read_frame(ctx, &pkt);
         if (pkt.stream_index == audio_index) {
             char adts_header_buf[7];
-            adts_header(adts_header_buf, pkt.size);
+            adts_header(adts_header_buf, pkt.size, aac_type, sample_rate, channels);
             fwrite(adts_header_buf, 1, 7, audio_file);
             len = fwrite(pkt.data, 1, pkt.size, audio_file);
             if (len != pkt.size) {
@@ -148,6 +221,7 @@ void extract_audio(const char *src, const char *dst) {
         }
         av_packet_unref(&pkt);
     }
+__ERROR:
     
     avformat_close_input(&ctx);
     if (audio_file) {
