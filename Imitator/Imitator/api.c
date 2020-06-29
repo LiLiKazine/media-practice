6526f3f9e1c10fd090705044ebc7a6fe0887d4a6
ShengLi <li.sheng@mobisport.cn>
2020-06-29 17:34:50 +0800

    add framework

---
+++ Imitator/Imitator/api.c
@@ -0,0 +1,9 @@
+//
+//  api.c
+//  Imitator
+//
+//  Created by Li Sheng on 2020/6/29.
+//  Copyright © 2020 lilikazine. All rights reserved.
+//
+
+#include "api.h"
---
+++ Imitator/Imitator/api.h
@@ -0,0 +1,14 @@
+//
+//  api.h
+//  Imitator
+//
+//  Created by Li Sheng on 2020/6/29.
+//  Copyright © 2020 lilikazine. All rights reserved.
+//
+
+#ifndef api_h
+#define api_h
+
+#include <stdio.h>
+#include "libavcodec/avcodec.h"
+#endif /* api_h */
